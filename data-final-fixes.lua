8226d735b9277ff5645fe2b0046c3bc9c206144f
Marshall Eastwood <142858715+TheFarawayDev@users.noreply.github.com>
2025-08-27 21:40:46 -0500

    Might Work

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,7 +2,7 @@ local instant_miners = settings.startup["instant-mining-miners"].value
 
 -- Make everything except miners and trees instant
 for prototype_type_name, prototype_type in pairs(data.raw) do
-  if prototype_type_name ~= "tree" and prototype_type_name ~= "mining-drill" and prototype_type_name ~= "burner-mining-drill" and prototype_type_name ~= "electric-mining-drill" then
+  if prototype_type_name ~= "tree" and prototype_type_name ~= "mining-drill" then
     for _, prototype in pairs(prototype_type) do
       if prototype.minable and type(prototype.minable) == "table" then
         prototype.minable.mining_time = 0.001
@@ -22,15 +22,12 @@ end
 
 -- Make miners instant if the setting is true
 if instant_miners then
-  local miner_types = {"mining-drill", "burner-mining-drill", "electric-mining-drill"}
-  for _, miner_type in ipairs(miner_types) do
-    if data.raw[miner_type] then
-      for _, prototype in pairs(data.raw[miner_type]) do
-        if prototype.minable and type(prototype.minable) == "table" then
-          prototype.minable.mining_time = 0.001
-        elseif prototype.minable == true then
-          prototype.minable = { mining_time = 0.001 }
-        end
+  if data.raw["mining-drill"] then
+    for _, prototype in pairs(data.raw["mining-drill"]) do
+      if prototype.minable and type(prototype.minable) == "table" then
+        prototype.minable.mining_time = 0.001
+      elseif prototype.minable == true then
+        prototype.minable = { mining_time = 0.001 }
       end
     end
   end
