85a25ef17f8d263be1c2e7bde47bbd42f6db7345
Marshall Eastwood <142858715+TheFarawayDev@users.noreply.github.com>
2025-08-27 21:23:43 -0500

    Fixes To Multiple Bugs

--- settings.lua
+++ settings.lua
@@ -7,7 +7,7 @@ data:extend(
     default_value = false,
     order = "a",
     per_user = false,
-    mod = "instant-mining",
+    mod = "instant-mining-plus",
     localization_settings_key = "instant-mining-miners-setting-name",
     localization_settings_key_description = "instant-mining-miners-setting-description"
   }
