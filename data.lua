8e9a7865f1a2e5a07b0e738cdf815166d63aad44
Marshall Eastwood <142858715+TheFarawayDev@users.noreply.github.com>
2025-08-27 20:43:28 -0500

    Fixes

--- data.lua
+++ data.lua
@@ -1,11 +1,9 @@
 for prototype_type_name, prototype_type in pairs(data.raw) do
-  if prototype_type_name ~= "tree" then
-    for _, prototype in pairs(prototype_type) do
-      if prototype.minable and type(prototype.minable) == "table" then
-        prototype.minable.mining_time = 0.01
-      elseif prototype.minable == true then
-        prototype.minable = { mining_time = 0.01 }
-      end
+  for _, prototype in pairs(prototype_type) do
+    if prototype.minable and type(prototype.minable) == "table" then
+      prototype.minable.mining_time = 0.01
+    elseif prototype.minable == true then
+      prototype.minable = { mining_time = 0.01 }
     end
   end
 end
