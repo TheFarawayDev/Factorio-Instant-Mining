e5c148524a07631165600ab4dc7610d30296dcb5
Marshall Eastwood <142858715+TheFarawayDev@users.noreply.github.com>
2025-08-27 21:37:24 -0500

    Minor Fixes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,11 +1,8 @@
-for _, prototype in pairs(data.raw["tree"]) do
-  if prototype.minable and type(prototype.minable) == "table" then
-    prototype.minable.mining_time = 0.001
-  end
-end
+local instant_miners = settings.startup["instant-mining-miners"].value
 
+-- Make everything except miners and trees instant
 for prototype_type_name, prototype_type in pairs(data.raw) do
-  if prototype_type_name ~= "tree" and prototype_type_name ~= "mining-drill" then
+  if prototype_type_name ~= "tree" and prototype_type_name ~= "mining-drill" and prototype_type_name ~= "burner-mining-drill" and prototype_type_name ~= "electric-mining-drill" then
     for _, prototype in pairs(prototype_type) do
       if prototype.minable and type(prototype.minable) == "table" then
         prototype.minable.mining_time = 0.001
@@ -16,12 +13,25 @@ for prototype_type_name, prototype_type in pairs(data.raw) do
   end
 end
 
-if settings.startup["instant-mining-miners"].value == true then
-  for _, prototype in pairs(data.raw["mining-drill"]) do
-    if prototype.minable and type(prototype.minable) == "table" then
-      prototype.minable.mining_time = 0.001
-    elseif prototype.minable == true then
-      prototype.minable = { mining_time = 0.001 }
+-- Make trees instant
+for _, prototype in pairs(data.raw["tree"]) do
+  if prototype.minable and type(prototype.minable) == "table" then
+    prototype.minable.mining_time = 0.001
+  end
+end
+
+-- Make miners instant if the setting is true
+if instant_miners then
+  local miner_types = {"mining-drill", "burner-mining-drill", "electric-mining-drill"}
+  for _, miner_type in ipairs(miner_types) do
+    if data.raw[miner_type] then
+      for _, prototype in pairs(data.raw[miner_type]) do
+        if prototype.minable and type(prototype.minable) == "table" then
+          prototype.minable.mining_time = 0.001
+        elseif prototype.minable == true then
+          prototype.minable = { mining_time = 0.001 }
+        end
+      end
     end
   end
 end
--- settings.lua
+++ settings.lua
@@ -7,7 +7,7 @@ data:extend(
     default_value = false,
     order = "a",
     per_user = false,
-    localised_name = {"[mod-setting-name].instant-mining-miners-setting-name"},
-    localised_description = {"[mod-setting-description].instant-mining-miners-setting-description"}
+    localised_name = {"mod-setting-name.instant-mining-miners-setting-name"},
+    localised_description = {"mod-setting-description.instant-mining-miners-setting-description"}
   }
 })
